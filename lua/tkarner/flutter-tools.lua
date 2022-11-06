b899c947b44cf66808d2985b3bc6c4099d5749c9
Thomas Karner <thomas.7.karner@gmail.com>
2022-11-06 11:58:06 +0100

    fix flutter lsp settings

--- lua/tkarner/flutter-tools.lua
+++ lua/tkarner/flutter-tools.lua
@@ -3,4 +3,8 @@ require("flutter-tools").setup({
 	dev_log = {
 		open_cmd = "tabedit",
 	},
+	lsp = {
+		on_attach = require("tkarner.lsp.handlers").on_attach,
+		capabilities = require("tkarner.lsp.handlers").capabilities,
+	},
 })
