4555010af3cd4162dcba4d54d162bc16d656023b
Thomas Karner <thomas.7.karner@gmail.com>
2023-04-01 16:28:55 +0200

    remap cmp suggestions and window navigation to support new alacritty bindings

--- lua/tkarner/keymaps.lua
+++ lua/tkarner/keymaps.lua
@@ -18,10 +18,10 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<Left>", "<C-w>h", opts)
+keymap("n", "<Right>", "<C-w>l", opts)
+keymap("n", "<Down>", "<C-w>j", opts)
+keymap("n", "<Up>", "<C-w>k", opts)
 
 -- Format keybinding like intellij
 keymap("n", "<C-A-l>", ":Format<cr>", opts)
