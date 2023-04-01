197108547b8a504a704295d642fa04dd26dddcbb
Thomas Karner <thomas.7.karner@gmail.com>
2023-04-01 10:05:12 +0200

    enable relative line numbers in nvim-tree

--- lua/tkarner/nvim-tree.lua
+++ lua/tkarner/nvim-tree.lua
@@ -42,8 +42,8 @@ nvim_tree.setup({
 				{ key = "v", cb = tree_cb("vsplit") },
 			},
 		},
-		number = false,
-		relativenumber = false,
+		number = true,
+		relativenumber = true,
 	},
 	filters = {
 		custom = {
